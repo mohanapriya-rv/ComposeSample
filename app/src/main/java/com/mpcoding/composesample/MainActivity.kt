297f5fd632e0359d599fb8924ce449c3eb5efacd
Mohanapriya <priya1151814@gmail.com>
2022-07-05 10:50:08 +0530

    Added Basic text and button functionality

--- app/src/main/java/com/mpcoding/composesample/MainActivity.kt
+++ app/src/main/java/com/mpcoding/composesample/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -56,7 +59,13 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 @Composable
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessHigh
+        )
+    )
     val context = LocalContext.current
     Surface(color = Color.Cyan) {
         Row(
@@ -64,7 +73,9 @@ fun Greeting(name: String) {
                 .padding(24.dp)
                 .fillMaxWidth()
         ) {
-            Column(modifier = Modifier.padding(bottom = extraPadding)) {
+            Column(
+                modifier = Modifier.padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
                 Text(
                     text = "Hello $name!", color = Purple200,
                     modifier = Modifier
--- app/src/main/java/com/mpcoding/composesample/OnBoardingActivity.kt
+++ app/src/main/java/com/mpcoding/composesample/OnBoardingActivity.kt
@@ -1,5 +1,9 @@
 package com.mpcoding.composesample
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,25 +12,29 @@ import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-class OnBoardingActivity {
+class OnBoardingActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            OnBoardingScreen()
+        }
+    }
 
     @Composable
     fun OnBoardingScreen() {
-        val shouldShowOnBoarding by remember { mutableStateOf(true) }
         Surface {
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Welcome")
+                Text("Welcome", color = Color.Black)
                 Button(modifier = Modifier.padding(vertical = 20.dp), onClick = { /*TODO*/ }) {
                     Text(text = "Continue")
                 }
